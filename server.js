009c2d2da980f09948ba8a14c917914e8b934293
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-08-11 21:42:19 -0500

    updated port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -49,8 +51,8 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
 const { animals } = require('./data/animals.json');
